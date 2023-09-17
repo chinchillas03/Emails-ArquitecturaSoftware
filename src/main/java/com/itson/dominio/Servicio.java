b51a64da3a403409f8d3e33391f63e1faf51bc12
CHINCHILLAS\Usuario <alvarezchinchillas3@gmail.com>
2023-09-17 00:40:46 -0700

    Se cambio el proyecto a maven

--- src/main/java/com/itson/dominio/Servicio.java
+++ src/main/java/com/itson/dominio/Servicio.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.dominio;
+package com.itson.dominio;
 
 import java.util.List;
 
@@ -38,5 +38,4 @@ public class Servicio {
         return cuentas.get(indice);
     }
 
-    
 }
