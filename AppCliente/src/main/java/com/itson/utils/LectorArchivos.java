1d01b96011fe0bce11a7d359aabfb3311cba0934
CHINCHILLAS\Usuario <alvarezchinchillas3@gmail.com>
2023-09-21 01:37:16 -0700

    se quitaron dependencias innecesarias

--- AppCliente/src/main/java/com/itson/utils/LectorArchivos.java
+++ AppCliente/src/main/java/com/itson/utils/LectorArchivos.java
@@ -4,12 +4,7 @@
  */
 package com.itson.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.itson.dominio.ConfiguracionCorreo;
-import com.itson.dominio.Cuenta;
-import com.itson.dominio.Servicio;
-import java.io.File;
-import java.io.IOException;
+
 
 /**
  *
@@ -18,26 +13,7 @@ import java.io.IOException;
 public class LectorArchivos {
     
     public void leerArchivo(){
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            // Leer el archivo JSON y convertirlo en un objeto ConfiguracionCorreo
-            ConfiguracionCorreo configuracion = objectMapper.readValue(new File("configuracion.json"), ConfiguracionCorreo.class);
-
-            // Acceder a los servicios de correo y trabajar con ellos
-            for (Servicio servicio : configuracion.getServiciosCorreo()) {
-                System.out.println("Nombre del servicio: " + servicio.getNombre());
-
-                for (Cuenta cuenta : servicio.getCuentas()) {
-                    System.out.println("Usuario: " + cuenta.getDireccion());
-                    System.out.println("Contraseña: " + cuenta.getContrasenia());
-                }
-
-                System.out.println("Protocolos: " + servicio.getProtocolos());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
     }
     
 }
